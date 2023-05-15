df73dd6b89d7852dbcc14d31bd1180ca8af81bc8
ayesha-python2023 <ayesha@protonmail.ch>
2023-05-15 11:09:02 -0600

    updated

--- BottlesForChange/settings.py
+++ BottlesForChange/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8^heh0li#$mvk-0ff5ii9tij)xto=7@zw#2k6*u+zfunf3bbnw'
+dotenv.load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
